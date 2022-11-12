4dfa6846de5369f87423ff133de514ddee80e756
benny <u3569037@connect.hku.hk>
2022-11-12 22:19:54 +0800

    First commit

---
+++ app/src/main/java/com/example/alertio/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.alertio
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.delay
+
+import java.util.*
+import kotlin.concurrent.schedule
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val intent = Intent(this@MainActivity, Homepage::class.java)
+        Timer().schedule(2000){
+            startActivity(intent)
+            finish()
+        }
+
+
+    }
+}
