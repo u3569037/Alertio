572059ecb60f3e79bb5c73fbd260c47c7966e4e1
benny <u3569037@connect.hku.hk>
2022-11-13 23:26:11 +0800

    More activities created for record and user guide

--- app/src/main/java/com/example/alertio/Homepage.kt
+++ app/src/main/java/com/example/alertio/Homepage.kt
@@ -1,11 +1,35 @@
 package com.example.alertio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import java.util.*
+import kotlin.concurrent.schedule
 
 class Homepage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        val guidebtn: Button = findViewById(R.id.guidebutton)
+        guidebtn.setOnClickListener {
+            val intent = Intent(this@Homepage, UserGuide::class.java)
+            startActivity(intent)
+        }
+
+        val recordbtn: Button = findViewById(R.id.recordbutton)
+        recordbtn.setOnClickListener {
+            val intent = Intent(this@Homepage, PastRecord::class.java)
+            startActivity(intent)
+        }
+
+        val startbtn: Button = findViewById(R.id.startbutton)
+        startbtn.setOnClickListener {
+            val intent = Intent(this@Homepage, Operating::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
---
+++ app/src/main/java/com/example/alertio/Operating.kt
@@ -0,0 +1,11 @@
+package com.example.alertio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Operating : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_operating)
+    }
+}
