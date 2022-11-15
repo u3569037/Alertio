f60eee5f2a7ba8816d7ba2d126c78c7dda0d0545
Elmo <james035@connect.hku.hk>
2022-11-15 19:30:59 +0800

    Past Record added with fake data. by Elmo

--- app/src/main/java/com/example/alertio/Operating.kt
+++ app/src/main/java/com/example/alertio/Operating.kt
@@ -2,10 +2,29 @@ package com.example.alertio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+
+import java.lang.Exception
+import java.io.File
+import java.io.FileOutputStream
 
 class Operating : AppCompatActivity() {
+    private val file = "mydata"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_operating)
+
+
+        // + System.getProperty("line.separator")
+        val data = ("12:23:29\n")
+        try {
+            val fOut: FileOutputStream = openFileOutput(file, MODE_APPEND)
+            fOut.write(data!!.toByteArray())
+            fOut.close()
+            Toast.makeText(getBaseContext(), "file saved", Toast.LENGTH_SHORT).show()
+        } catch (e: Exception) {
+         e.printStackTrace()
+        }
     }
+
 }
--- app/src/main/java/com/example/alertio/PastRecord.kt
+++ app/src/main/java/com/example/alertio/PastRecord.kt
@@ -1,11 +1,51 @@
 package com.example.alertio
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ListView
+import android.widget.SimpleAdapter
+import android.widget.Toast
+import java.io.FileInputStream
+import java.io.File
+import java.io.FileOutputStream
+
 
 class PastRecord : AppCompatActivity() {
+    private val file = "mydata"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_past_record)
+        var recordList = arrayOf<String>()
+        try {
+            val fin: FileInputStream = openFileInput(file)
+            var c: Int
+            var temp = ""
+            while (fin.read().also { c = it } != -1) {
+                temp = temp + Character.toString(c.toChar())
+                if(c.toChar() == '\n'){
+                    recordList += temp
+                    temp = ""
+                }
+            }
+
+        }
+            catch (e: Exception) {
+        }
+
+
+        val list = arrayListOf<MutableMap<String, Any>>()
+        for (i in recordList!!.indices) {
+            val map: MutableMap<String, Any> = HashMap()
+            val count = i+1
+            map["Danger"] = "Danger $count"
+            map["Time"] = recordList[i]
+            list.add(map)
+        }
+        val adapter = SimpleAdapter(this, list, R.layout.activity_list_item,
+            arrayOf("Danger", "Time"), intArrayOf(R.id.role, R.id.name))
+        val list_view: ListView = findViewById(R.id.list_view)
+        list_view.adapter = adapter }
     }
-}
+
