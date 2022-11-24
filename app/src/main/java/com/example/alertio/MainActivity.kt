07c6a6b00326bab2efa8df8c4fe1f09e985b4bfc
Benny <u3569037@connect.hku.hk>
2022-11-25 00:05:57 +0800

    Bug fix for loading app

--- app/src/main/java/com/example/alertio/MainActivity.kt
+++ app/src/main/java/com/example/alertio/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         Timer().schedule(2000){
             startActivity(intent)
             finish()
+            return@schedule
         }
     }
 }
